1ff4735d10276bf85d41281bcef8fafc9b3c003e
ctdk <jbingham@gmail.com>
2015-10-16 12:37:35 -0700

    didn't get metadata merged right

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 name		 "dwarf_fortress"
 maintainer       "Jeremy Bingham"
 maintainer_email "jbingham@gmail.com"
@@ -6,15 +5,6 @@ license          "MIT"
 description      "Installs/Configures dwarf_fortress"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.3.0"
-=======
-maintainer 'Jeremy Bingham'
-maintainer_email 'jbingham@gmail.com'
-license 'MIT'
-description 'Installs/Configures dwarf_fortress'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.2'
-name 'dwarf-fortress'
->>>>>>> lazynewb
 
 %w(mac_os_x debian).each do |os|
   supports os
@@ -32,4 +22,4 @@ attribute 'df/source',
 attribute 'df/version',
           display_name: 'Dwarf Fortress version',
           description: 'Dwarf Fortress version string',
-          default: 'df_0.34.10'
+          default: 'df_0.40.24'
